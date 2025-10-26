11b040da7d41e953bb9941248ff9e483714af8b6
Matteuszhrv <horvath.mate1891@gmail.com>
2025-10-26 10:09:31 +0100

    Delete launch/__pycache__ directory

--- launch/__pycache__/minmax_launch.py
+++
@@ -1,8 +0,0 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
-
-def generate_launch_description():
-    return LaunchDescription([
-        Node(package='mypack', executable='number_publisher', output='screen'),
-        Node(package='mypack', executable='minmax_subscriber', output='screen'),
-    ])
